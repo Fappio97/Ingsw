331ae78eb4997625775456d23769657719a895ff
Fabio <75536496+Fiabio97@users.noreply.github.com>
2021-11-14 16:44:31 +0100

    Add files via upload
    Aggiornameto + commento

--- Design Pattern/PatternStrutturali/Adapter/src/adapter/Main.java
+++ Design Pattern/PatternStrutturali/Adapter/src/adapter/Main.java
@@ -4,9 +4,12 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
+		Target target = new Target("Casa");
+		System.out.println(target.stampa());
+		
 		Adaptee adaptee = new Adaptee(Arrays.asList(67, 97, 115, 97));
 		
-		Target target = new Adapter(adaptee);
+		target = new Adapter(adaptee);
 		System.out.println(target.stampa());
 	}
 }
